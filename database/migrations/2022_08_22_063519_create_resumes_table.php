fe4b9412af4e235eeb956f93a4cadf11d695003b
Adi Ferdian <adiferdian7@gmail.com>
2022-08-22 13:57:52 +0700

    Migrate table

---
+++ database/migrations/2022_08_22_063519_create_resumes_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateResumesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('resumes', function (Blueprint $table) {
+            $table->id();
+            $table->string('category');
+            $table->string('title');
+            $table->text('desc');
+            $table->text('learn');
+            $table->string('year_in');
+            $table->string('year_out');
+            $table->string('place');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('resumes');
+    }
+}
